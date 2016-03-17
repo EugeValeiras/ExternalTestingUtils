c21d18cb7d1b5096632a7ce82fb3baaf31483673
Eugenio Jesus Jose Valeiras <Eugenio.valeiras@gmail.com>
2016-03-17 11:26:27 -0300

    init commit

---
+++ src/main/java/externaltesting/utils/ExternalTestRunner.java
@@ -0,0 +1,20 @@
+package externaltesting.utils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public interface ExternalTestRunner {
+
+	public String getRemoteTestingUsername();
+	public void setRemoteTestingUsername(String username);
+	public String getRemoteTestingPassword();
+	public void setRemoteTestingPassword(String password);
+	
+	public RemoteWebDriver getRemoteWebDriver(String sessionName, TestEnvironment testEnvironment) throws MalformedURLException;
+	public void changeStatusOfJob(String jobID, Boolean generalStatus) throws MalformedURLException, IOException, JSONException;
+	public String getPublicVideoLinkOfJob(String jobID) throws IOException, JSONException;
+	
+}
